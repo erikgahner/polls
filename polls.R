361c3881410324b25b57195948797a7bf4bb9007
Erik Gahner Larsen <erikgahner@gmail.com>
2015-05-07 01:02:08 -0400

    update script
    new theme and variable name for confidence intervals

--- polls.R
+++ polls.R
@@ -19,19 +19,16 @@ polls$date <- format(as.Date(c(paste(polls$year,
 polls <- polls[polls$date > as.Date("2011-09-15"),]
 
 # Calcuate standard error for Venstre
-polls$se_v <- 1.96 * sqrt( (polls$party_v * (100-polls$party_v) ) / polls$n)  
+polls$ci_v <- 1.96 * sqrt( (polls$party_v * (100-polls$party_v) ) / polls$n)  
 
 # Plot polls for Venstre
 ggplot(polls, aes(x=as.Date(date), y=party_v)) + 
-  geom_errorbar(aes(ymin=party_v-se_v, ymax=party_v+se_v), color="gray90") +
+  geom_smooth(aes(ymin=party_v-ci_v, ymax=party_v+ci_v), 
+              fullrange=T, 
+              linetype=0,
+              stat="identity") +
   geom_point(shape=1) + 
   ggtitle("Venstre") +
   ylab("Stemmer (%)") +
   xlab("") +
-  theme_bw() + 
-  theme(axis.line = element_line(colour = "black"), 
-        panel.grid.major = element_line(color = "gray80"),
-        panel.grid.major.x = element_blank(),
-        panel.grid.minor = element_blank(), 
-        panel.border = element_blank(), 
-        panel.background = element_blank())
+  theme_minimal()
