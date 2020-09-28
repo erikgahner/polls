9c314291a9cb3b6a48d898b4d28821327d58dd59
Erik Gahner Larsen <erikgahner@gmail.com>
2020-09-28 10:06:50 +0100

    remove Veganerpartiet from figure with all parties

--- polls.R
+++ polls.R
@@ -129,14 +129,17 @@ dev.off()
 png('figs/support-all.png', width = 800, height = 700, units = "px", res = 115)
 polls_use %>%
   gather(party, support, party_a:party_aa) %>%
+  filter(party != "party_g") %>% 
   ggplot(aes(x=as.Date(date), y=support, colour=party)) +
   geom_point(size=1, alpha=0.3) +
   geom_smooth(se=FALSE, method="loess", span = .3) +
   geom_hline(yintercept=2, linetype = "dashed") +
   labs(y = "Stemmer (%)",
        x = NULL) +
-  scale_colour_manual(labels = c("Socialdemokraterne", "Alternativet", "Radikale Venstre", "Konservative", "Nye Borgerlige", "Klaus Riskær Pedersen", "SF", "Veganerpartiet", "Liberal Alliance", "Kristendemokraterne", "Dansk Folkeparti", "Enhedslisten", "Stram Kurs", "Venstre"), 
-                      values = c("#E3515D", "#AEFEAF", "#EB4295", "#429969", "#05454F", "#537D7A", "#9C1D2A", "green", "#EE9A5F", "#F4CE97", "#3D6F8D", "#914A4F", "#000000", "#459BC8"),
+  scale_colour_manual(labels = c("Socialdemokraterne", "Alternativet", "Radikale Venstre", "Konservative", "Nye Borgerlige", "Klaus Riskær Pedersen", "SF", #"Veganerpartiet", 
+                                 "Liberal Alliance", "Kristendemokraterne", "Dansk Folkeparti", "Enhedslisten", "Stram Kurs", "Venstre"), 
+                      values = c("#E3515D", "#AEFEAF", "#EB4295", "#429969", "#05454F", "#537D7A", "#9C1D2A", #"green",
+                                 "#EE9A5F", "#F4CE97", "#3D6F8D", "#914A4F", "#000000", "#459BC8"),
                       guide = guide_legend(ncol = 4)) +
   theme_minimal(base_size = 12, base_family = "Barlow") %+replace% 
   theme(panel.grid.major.x = element_blank(), 
